278890dc489efb29c3eeb88ebd1cecbdac5b122d
Brayan Noé <84888260+Brashan7u7@users.noreply.github.com>
2024-05-19 08:31:13 -0600

    actividad

--- backend/app/Http/Controllers/profesorController.php
+++ backend/app/Http/Controllers/profesorController.php
@@ -15,4 +15,30 @@ class profesorController extends Controller
         $profesores = profesor::all();
         return response()->json($profesores, 200);
     }
+
+    public function getProfesor($id){
+        $profesor = profesor::find($id);
+        if(!$profesor){
+            return response()->json(['message' => 'Profesor no encontrado'], 404);
+        }
+        return response()->json($profesor, 200);
+    }
+
+    public function deleteProfesor($id){
+        $profesor = profesor::find($id);
+        if(!$profesor){
+            return response()->json(['message' => 'Profesor no encontrado'], 404);
+        }
+        $profesor->delete();
+        return response()->json(['message' => 'Profesor eliminado'], 200);
+    }
+
+    public function updateProfesor(request $request, $id){
+        $profesor = profesor::find($id);
+        if(!$profesor){
+            return response()->json(['message' => 'Profesor no encontrado'], 404);
+        }
+        $profesor->update($request->all());
+        return response()->json($profesor, 200);
+    }
 }
