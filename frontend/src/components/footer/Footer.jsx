0fe8bfb4e0d8078ba6a9eb13598406655739767f
rocimed <romaroci@hotmail.com>
2024-05-18 23:30:22 -0600

    front change

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./footer.module.css"
+const Footer =()=>{
+    return(
+    <div className={styles.container}>
+        <div className={styles.column}>
+        <img src="/logo.png" width="150" height="120" alt="" />
+        </div>
+        <div className={styles.column2}>
+        <img src="/contacto.png" width="150" height="120" alt="" />
+        <h3>Contactanos</h3>
+        </div>
+        <div className={styles.column3}>
+            <p>Santa Cruz Xoxocotlán, Oaxaca</p>
+            <p>Politicas de Privacidad</p>
+        </div>
+        <div className={styles.column4}>
+            <h3>Siguenos</h3>
+        </div>
+    </div>
+    )
+}
+export default Footer
